501ab062a72eb78493377334193ba54509a9cbcf
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-22 02:28:41 +0000

    remove destroy from categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -20,16 +20,6 @@ class Admin::CategoriesController < Admin::AuthController
     end
   end
 
-  def destroy
-    @category = Category.find params[:id]
-    if @category.destroy
-      redirect_to [:admin, :categories],
-      notice: 'Category deleted!'
-    else
-      redirect_to [:admin, :categories], notice: 'Category not empty! Remove products before deleting category.'
-    end
-  end
-
   private
 
   def category_params
