12f9db9516cf9d35d0d8764bc03ec680adb8087c
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-22 16:33:15 +0000

    Login using email and password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_email params[:session][:email]
     # If the user exists AND the password entered is correct.
-    if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:session][:password])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = @user.id
